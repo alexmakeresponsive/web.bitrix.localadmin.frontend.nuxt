41d57f37f21cf347c9d690b4a1179d19dbe1a24b
amr <alexmakeresponsive@gmail.com>
2020-09-15 02:17:24 +0600

    component: MenuMain: add logout method

--- components/dropdown/MenuMain.vue
+++ components/dropdown/MenuMain.vue
@@ -9,13 +9,72 @@
       <div class="dropdown-divider"></div>
       <div class="dropdown-item">
         Имя Фамилия
-        <NuxtLink to="/logout" type="button">[Выход]</NuxtLink>
+        <span v-on:click="logout" type="button">[Выход]</span>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: "menu-main"
+  name: "menu-main",
+  methods: {
+    logout: async function (e) {
+      try
+      {
+        await fetch(
+            'http://192.168.100.6/api/auth/logout',
+            {
+              method: 'GET',
+            }
+        ).then(response => {
+          // console.log(response)
+
+          if(!response)
+          {
+            console.log('many req per sec');
+            return {};
+          }
+
+          if(response.status !== 200)
+          {
+            console.log('server unavailable');
+            return {};
+          }
+
+          return response.json();
+        })
+            .then(data => {
+              console.log(data);
+
+              if (!data.hasOwnProperty('status'))
+              {
+                console.log("status error")
+                return;
+              }
+
+              if (data.status !== 200)
+              {
+                console.log('error when logout')
+                return;
+              }
+
+
+              this.$store.commit('http/updateRedirectPathFirst', "");
+              this.$store.commit('token/updateCsrf', "");
+
+              this.$store.commit('spaces/updateSpaceList', []);
+
+              window.location = '/local/admin/'
+            });
+      }
+      catch (e)
+      {
+        console.log(e);
+        alert("catch!");
+        redirect(`/error/http/error`);
+      }
+    }
+  }
+
 }
 </script>
